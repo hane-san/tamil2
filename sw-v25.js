0ca55400adcc55a8e787ef72b7116e13d5917261
hane-san <shun.y.0127@gmail.com>
2026-07-20 14:36:51 +0900

    Add v2.5 offline cache

---
+++ sw-v25.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = "tamil2-spoken-tamil-v25-20260720";
+const APP_FILES = [
+  "./",
+  "./index.html",
+  "./styles-v25.css",
+  "./curriculum-v25.js",
+  "./travel-v25.js",
+  "./app-v25.js",
+  "./app-icon.png",
+  "./manifest-v25.webmanifest"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
+      const copy = response.clone();
+      caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+      return response;
+    }))
+  );
+});
